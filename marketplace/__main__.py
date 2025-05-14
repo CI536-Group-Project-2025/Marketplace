b2357074c90878182faadd0688464c614524d68e
Ben Harris <benharrisdevelopment@gmail.com>
2025-05-14 14:00:05 +0100

    Changed /signup to POST to use forms. Added placeholder form. Implemented full address storage.

--- marketplace/__main__.py
+++ marketplace/__main__.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 import psycopg2
 from marketplace import item, users
 
-app = Flask("Marketplace")
+app = Flask(__name__)
+
+# Session config - for keeping users logged in.
+# This will log users out when they close the browser
+app.config["SESSION_PERMANENT"] = False
+# This will store session data on the filesystem
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 app.register_blueprint(item.bp)
 app.register_blueprint(users.bp)
