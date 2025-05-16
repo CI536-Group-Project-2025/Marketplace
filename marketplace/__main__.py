e27d47dd769aa60f96926fc381604af01bc83550
Ben Harris <benharrisdevelopment@gmail.com>
2025-05-16 01:32:54 +0100

    Basic basket page

--- marketplace/__main__.py
+++ marketplace/__main__.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template
 from flask_session import Session
-import psycopg2
-from marketplace import item, users
+from marketplace import basket, item, users
+
 
 app = Flask(__name__)
 
@@ -19,6 +19,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+app.register_blueprint(basket.bp)
 app.register_blueprint(item.bp)
 app.register_blueprint(users.bp)
 
